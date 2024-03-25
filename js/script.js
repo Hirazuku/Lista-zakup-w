12c5934c338edfe7c2593300e426acb3424069da
Michał Kowalczyk <michalkowal3k@gmail.com>
2024-03-25 20:09:55 +0100

    adding new tasks to list

--- js/script.js
+++ js/script.js
@@ -1,18 +1,72 @@
 {
-    const calculateValue = (event) => {
+    const tasks = [
+        {
+            content: "Warzywa i owoce",
+            done: false,
+        },
+        {
+            content: "Nabiał",
+            done: false,
+        },
+        {
+            content: "Przyprawy",
+            done: false,
+        },
+        {
+            content: "Mięso",
+            done: false,
+        },
+        {
+            content: "Zboże",
+            done: false,
+        },
+        {
+            content: "Desery",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li
+            ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+            ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    }
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+
+    const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const amount = document.querySelector(".js-A");
-        const exchangeRate = document.querySelector(".js-eR");
-        const value = amount.value * exchangeRate.value;
-        const finalValue = document.querySelector(".js-finalValue");
-        finalValue.innerText = value;
-    };
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    }
 
     const init = () => {
-        const money = document.querySelector(".js-currency");
-        money.addEventListener("submit", calculateValue)
-    };
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+
+    }
 
     init();
 }
